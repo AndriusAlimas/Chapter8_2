2b40ec5a3ddadfc39b01243ca2caef7a5cddf45f
Andrius <andriusjavait@gmail.com>
2017-02-01 21:51:13 +0000

    update index.html with form added MyController servlet template code update DD servlet mapping

---
+++ Chapter8_2/src/com/example/servlet/MyController.java
@@ -0,0 +1,11 @@
+package com.example.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class MyController extends HttpServlet{
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse reponse)throws ServletException, IOException{
+		
+	}
+}
