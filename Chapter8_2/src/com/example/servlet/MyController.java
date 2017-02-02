d315c922b1378b2714edddef5c6728c5760c871a
Andrius <andriusjavait@gmail.com>
2017-02-02 21:32:15 +0000

    update servlet code

--- Chapter8_2/src/com/example/servlet/MyController.java
+++ Chapter8_2/src/com/example/servlet/MyController.java
@@ -1,11 +1,19 @@
 package com.example.servlet;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import foo.other.*;
 
-public class MyController extends HttpServlet{
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse reponse)throws ServletException, IOException{
+
+public class MyController extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String p_name = request.getParameter("person_name");
+		Person person = new Person();
+		request.setAttribute("person_object", (Object) person);
 		
+		RequestDispatcher rd = request.getRequestDispatcher("go.jsp");
+		rd.forward(request, response);
 	}
 }
